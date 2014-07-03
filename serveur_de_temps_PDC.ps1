90253ae47c44a65e0412f68378025947407ace71
Camille Ollié <camille.ollie@ac-montpellier.fr>
2014-07-03 10:22:52 +0200

    Serveur NTP PDC

---
+++ serveur_de_temps_PDC.ps1
@@ -0,0 +1,17 @@
+﻿if ( (Test-Path HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters) -eq 'True' ) {
+
+     Push-Location
+ 
+     Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters
+ 
+     if ( (Get-ItemProperty . NtpServer) -ne '10.134.62.1,0x1' ) {
+
+        Set-ItemProperty . NtpServer "10.134.62.1,0x1"
+
+    }
+
+    Pop-Location
+
+    Restart-Service "W32Time"
+ }
+
