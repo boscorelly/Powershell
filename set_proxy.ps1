83ad980c713241d67dd88a58197c7ef8f1c29bc3
Camille Ollié <camille.ollie@ac-montpellier.fr>
2014-10-10 09:59:59 +0200

    add proxy script

---
+++ set_proxy.ps1
@@ -0,0 +1,13 @@
+﻿$regkey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+$proxy = "10.34.62.1:3128"
+
+Push-Location
+ 
+Set-Location $regkey
+
+if ( (Get-ItemProperty . ProxyEnable) -ne '0' ) {
+    Set-ItemProperty . ProxyEnable -Value 1
+    Set-ItemProperty . ProxyServer -Value $proxy 
+}
+
+Pop-Location
