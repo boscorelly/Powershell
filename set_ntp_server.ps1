73421ac91b12fda4ef1abd43799d6778f11c5cbe
Camille Ollié <camille.ollie@ac-montpellier.fr>
2014-10-10 13:27:28 +0200

    Complete rewrite
    Complete rewrite of ntp server configuration

--- set_ntp_server.ps1
+++ set_ntp_server.ps1
@@ -1,17 +1,44 @@
-﻿if ( (Test-Path HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters) -eq 'True' ) {
+﻿$ntpserver = "10.134.62.1"
+$hklmsvc = "HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters"
+$hklmsrv = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers"
+
+if ( (Test-Path $hklmsvc) -eq 'True' ) {
 
      Push-Location
  
-     Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters
+     Set-Location $hklmsvc
  
-     if ( (Get-ItemProperty . NtpServer) -ne '10.134.62.1,0x1' ) {
+     if ( (Get-ItemProperty . NtpServer) -ne "$ntpserver,0x1" ) {
 
-        Set-ItemProperty . NtpServer "10.134.62.1,0x1"
+        Write-Host "Configuring NTP service."
+
+        Set-ItemProperty . NtpServer "$ntpserver,0x1"
 
     }
 
     Pop-Location
 
-    Restart-Service "W32Time"
  }
 
+if ( (Get-ItemProperty -Path $hklmsrv) -notmatch $ntpserver) {
+
+    Write-Host "Server not found. Adding."
+
+    Push-Location
+
+    Set-Location $hklmsrv
+
+    Set-ItemProperty . 50 -Value "$ntpserver"
+}
+
+if ( (Get-ItemProperty . "(default)") -ne "50") {
+    
+    Write-Host "Setting default NTP Server."
+
+    Set-ItemProperty . "(default)" "50"
+}
+
+Pop-Location
+
+Stop-Service "W32Time"
+Start-Service "W32Time"
