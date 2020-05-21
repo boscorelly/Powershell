efd5282af0a81f0bda283c57b647d5cc8875d2ba
Camille Ollié <bosco.relly@laposte.net>
2020-05-21 17:08:46 +0200

    Update

--- set_ntp_server.ps1
+++ set_ntp_server.ps1
@@ -3,17 +3,12 @@ $hklmsvc = "HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters"
 $hklmsrv = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers"
 
 if ( (Test-Path $hklmsvc) -eq 'True' ) {
-
      Push-Location
- 
      Set-Location $hklmsvc
  
      if ( (Get-ItemProperty . NtpServer) -ne "$ntpserver,0x1" ) {
-
         Write-Host "Configuring NTP service."
-
         Set-ItemProperty . NtpServer "$ntpserver,0x1"
-
     }
 
     Pop-Location
@@ -21,20 +16,14 @@ if ( (Test-Path $hklmsvc) -eq 'True' ) {
  }
 
 if ( (Get-ItemProperty -Path $hklmsrv) -notmatch $ntpserver) {
-
     Write-Host "Server not found. Adding."
-
     Push-Location
-
     Set-Location $hklmsrv
-
     Set-ItemProperty . 50 -Value "$ntpserver"
 }
 
-if ( (Get-ItemProperty . "(default)") -ne "50") {
-    
+if ( (Get-ItemProperty . "(default)") -ne "50") { 
     Write-Host "Setting default NTP Server."
-
     Set-ItemProperty . "(default)" "50"
 }
 
